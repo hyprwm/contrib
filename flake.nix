0c84dd585d1039e0017c7f5263db0ec82ee7a8e1
Mihai Fufezan <fufexan@protonmail.com>
2022-07-20 18:31:07 +0300

    nix: add initial flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Hypr Development Contrib";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    genSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+    ];
+    pkgsFor = nixpkgs.legacyPackages;
+  in {
+    overlays.default = _: prev: {};
+
+    packages = genSystems (system: self.overlays.default null pkgsFor.${system});
+
+    formatter = genSystems (system: pkgsFor.${system}.alejandra);
+  };
+}
