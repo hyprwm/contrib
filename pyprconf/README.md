aa10bd3b54bd259ac344ef87a7514468c5d236b1
trippwill <5862883+trippwill@users.noreply.github.com>
2022-10-23 17:04:32 +0100

    pyprconf: add installation

---
+++ pyprconf/README.md
@@ -0,0 +1,20 @@
+# pyprconf
+
+**templated configuration and event handling for Hyprland**
+
+pyprconf is a front-end for Hyprland socket.sock and socket2.sock.
+It is configured using a yaml file, and is intended to be exec'd in your
+`hyprland.conf`.
+
+## Installation
+
+`python -m pip install .`
+
+Modules will be installed to appropiate `site-packages` location, and a
+command-line tool `pyprconf` in $HOME/.local/bin`.
+
+### `hyprland.conf`
+
+```
+exec-once pyprconf /path/to/conf.yaml
+```
--- pyprconf/pyprconf/pyprconf.py
+++ pyprconf/pyprconf/pyprconf.py
@@ -7,7 +7,7 @@
 #   pyrpconf /path/to/conf1.yaml /path/to/conf2.yaml
 #
 # Add the command to hyprland.conf using exec-once (probably toward the bottom).
-# 
+#
 # The configuration file must be in yaml, but support for other formats would
 # be pretty easy to add, as long as they parse to a dict of the same shape.
 
@@ -22,11 +22,9 @@ import yaml
 import configuration
 from configuration import Configuration, EventHandler
 
-SOCK1_ADDR = '/tmp/hypr/' + \
-    os.environ['HYPRLAND_INSTANCE_SIGNATURE'] + '/.socket.sock'
+SOCK1_ADDR = "/tmp/hypr/" + os.environ["HYPRLAND_INSTANCE_SIGNATURE"] + "/.socket.sock"
 
-SOCK2_ADDR = '/tmp/hypr/' + \
-    os.environ['HYPRLAND_INSTANCE_SIGNATURE'] + '/.socket2.sock'
+SOCK2_ADDR = "/tmp/hypr/" + os.environ["HYPRLAND_INSTANCE_SIGNATURE"] + "/.socket2.sock"
 
 
 files: Union[List[str], None] = None
@@ -51,34 +49,34 @@ def check_gate(name: str):
 
 
 def send(msg: bytes):
-    log(f'=> {msg}')
+    log(f"=> {msg}")
     if not msg:
         return
     try:
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         sock.connect(SOCK1_ADDR)
     except socket.error as err:
-        log(f'connect error: {err} ')
+        log(f"connect error: {err} ")
         return
 
     try:
         sock.sendall(msg)
         data = sock.recv(2048).decode()
-        log(f'<= {data}')
+        log(f"<= {data}")
         return data
     except socket.error as err:
-        log(f'send error: {err}')
+        log(f"send error: {err}")
     finally:
         sock.close()
 
 
 def configure():
     global config, enabled
-    log(f'loading configuration: {files}')
+    log(f"loading configuration: {files}")
 
     yamldoc = dict()
     for file in files:
-        with open(file, mode='rb') as f:
+        with open(file, mode="rb") as f:
             yamldoc.update(yaml.load(f, Loader=yaml.CSafeLoader))
 
     loaders, config = configuration.parse(yamldoc, logger=log)
@@ -94,7 +92,7 @@ def reload(*_):
 
 def unload(*_):
     global enabled
-    log(f'unloading configuration')
+    log(f"unloading configuration")
     enabled = False
     if config:
         for unloader in config.unloaders:
@@ -104,23 +102,23 @@ def unload(*_):
 def process_handler(send: Callable[[bytes], str], handler: EventHandler):
     if handler.check:
         if not check_gate(handler.check):
-            log(f'gate check failed: {handler.check}')
+            log(f"gate check failed: {handler.check}")
             return
         else:
-            log(f'gate check passed: {handler.check}')
+            log(f"gate check passed: {handler.check}")
     if handler.set:
         update_gate(handler.set, True)
-        log(f'set gate: {handler.set}')
+        log(f"set gate: {handler.set}")
     if handler.reset:
         update_gate(handler.reset, False)
-        log(f'reset gate: {handler.reset}')
+        log(f"reset gate: {handler.reset}")
     if handler.send:
         send(handler.send)
 
 
-def main(argv):
+def main():
     global files
-    files = argv[1:]
+    files = sys.argv[1:]
     configure()
     signal.signal(signal.SIGUSR1, reload)
     signal.signal(signal.SIGUSR2, unload)
@@ -133,27 +131,27 @@ def main(argv):
 
         while enabled:
             if not config.handlers:
-                log('no handlers registered, waiting for config reload...')
+                log("no handlers registered, waiting for config reload...")
                 signal.pause()
                 continue
 
             line = sock2file.readline()
             if not enabled:
                 continue
-            name, data = line.rstrip().rsplit('>>')
+            name, data = line.rstrip().rsplit(">>")
             if name in config.handlers:
                 for pattern, eventhandlers in config.handlers[name].items():
                     if pattern.fullmatch(data):
-                        log(f'matched event <{name}> {pattern.pattern}: {data}')
+                        log(f"matched event <{name}> {pattern.pattern}: {data}")
                         for eventhandler in eventhandlers:
                             process_handler(send, eventhandler)
 
     except KeyboardInterrupt:
-        log('stopped with keyboard interrupt')
+        log("stopped with keyboard interrupt")
         unload()
     finally:
         sock2.close()
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
