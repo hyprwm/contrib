5f4d47c6950375c4e3e36d4fd9861abba53eca4c
Mihai Fufezan <fufexan@protonmail.com>
2023-02-27 14:37:53 +0200

    scratchpad: add nix package

---
+++ scratchpad/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenvNoCC,
+  makeWrapper,
+  libnotify,
+  gnused,
+  jq,
+  bash,
+  hyprland ? null,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "scratchpad";
+  version = "0.1";
+  src = ./.;
+
+  buildInputs = [bash];
+  makeFlags = ["PREFIX=$(out)"];
+  nativeBuildInputs = [makeWrapper];
+
+  postInstall = ''
+    wrapProgram $out/bin/scratchpad --prefix PATH ':' \
+      "${lib.makeBinPath ([
+        gnused
+        jq
+        libnotify
+      ]
+      ++ lib.optional (hyprland != null) hyprland)}"
+  '';
+
+  meta = with lib; {
+    description = "A helper for scratchpads within Hyprland";
+    license = licenses.mit;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [fufexan];
+  };
+}
