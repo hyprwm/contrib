9c4057e6f667e7f7cd043bc8ec3ee4a473f360f0
Mihai Fufezan <mihai@fufexan.net>
2025-09-14 20:36:00 +0300

    grimblast/screenshot.sh: fix menu app selection

--- grimblast/screenshot.sh
+++ grimblast/screenshot.sh
@@ -22,45 +22,45 @@ action_opts=(" Copy" " Save" " Copy &amp; Save" " Edit")
 
 # Detect available menu program
 define_menu() {
-    if [[ -z $menu_command ]]; then
-        if command -v wofi >/dev/null 2>&1; then
-            menu_command="wofi"
-        elif command -v rofi >/dev/null 2>&1; then
-            menu_command="rofi"
-        elif command -v walker >/dev/null 2>&1; then
-            menu_command="walker"
-        else
-            echo "No menu program found (wofi, rofi, walker)" >&2
-            exit 1
-        fi
+  if ! command -v $menu_command; then
+    if command -v wofi >/dev/null 2>&1; then
+      menu_command="wofi"
+    elif command -v rofi >/dev/null 2>&1; then
+      menu_command="rofi"
+    elif command -v walker >/dev/null 2>&1; then
+      menu_command="walker"
+    else
+      echo "No menu program found (wofi, rofi, walker)" >&2
+      exit 1
     fi
+  fi
 }
 
 # Show menu based on detected command
 show_menu() {
-    prompt="$1"
-    shift
-    menu_opts=("$@") # global array
-    case $menu_command in
-    "wofi") wofi_menu ;;
-    "rofi") rofi_menu ;;
-    "walker") walker_menu ;;
-    esac
+  prompt="$1"
+  shift
+  menu_opts=("$@") # global array
+  case $menu_command in
+  "wofi") wofi_menu ;;
+  "rofi") rofi_menu ;;
+  "walker") walker_menu ;;
+  esac
 }
 
 wofi_menu() {
-    printf '%s\n' "${menu_opts[@]}" | wofi --dmenu --prompt "$prompt" -m
+  printf '%s\n' "${menu_opts[@]}" | wofi --dmenu --prompt "$prompt" -m
 }
 
 rofi_menu() {
-    printf '%s\n' "${menu_opts[@]}" | rofi \
-        -dmenu \
-        -p "$prompt" \
-        -markup-rows
+  printf '%s\n' "${menu_opts[@]}" | rofi \
+    -dmenu \
+    -p "$prompt" \
+    -markup-rows
 }
 
 walker_menu() {
-    printf '%s\n' "${menu_opts[@]}" | walker -p "$prompt" -d
+  printf '%s\n' "${menu_opts[@]}" | walker -p "$prompt" -d
 }
 
 define_menu
@@ -81,8 +81,8 @@ case "$type_choice" in
 esac
 
 if [[ $mode == "timer" ]]; then
-    timer_choice=$(show_menu "Choose Timer" "${timer_opts[@]}") || exit 1
-    countdown=${timer_choice%s} # remove 's'
+  timer_choice=$(show_menu "Choose Timer" "${timer_opts[@]}") || exit 1
+  countdown=${timer_choice%s} # remove 's'
 fi
 
 action_choice=$(show_menu "Action" "${action_opts[@]}") || exit 1
@@ -95,18 +95,18 @@ esac
 
 # Timer function
 timer() {
-    for ((i = countdown; i > 0; i--)); do
-        #First time and 10 last seconds
-        if ((i <= 10 || i == countdown)); then
-            notify-send -t 1000 "Taking Screenshot in $i"
-        fi
-        sleep 1
-    done
+  for ((i = countdown; i > 0; i--)); do
+    #First time and 10 last seconds
+    if ((i <= 10 || i == countdown)); then
+      notify-send -t 1000 "Taking Screenshot in $i"
+    fi
+    sleep 1
+  done
 }
 
 # Take screenshot
 takescreenshot() {
-    grimblast --notify "$option_action" "$option_type"
+  grimblast --notify "$option_action" "$option_type"
 }
 
 [[ $mode == "timer" ]] && timer
